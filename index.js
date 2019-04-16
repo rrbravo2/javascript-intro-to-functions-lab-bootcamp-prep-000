3a2810125a527a00be240f7c67207c140679e3aa
Ryan Bravo <rrbravo2@gmail.com>
2019-04-16 09:00:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string) {
-    return "I can't hear you!";
+  if(string.toLowerCase() === string) 
+    return "I can't hear you!"; }
   
-  } else if (string.toUpperCase() === string) {
-    return "Yes indeed!";
     
     
   
